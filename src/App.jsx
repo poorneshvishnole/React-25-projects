ea91904f2f042e7b6e84a2ef2aa990a291a435ef
poorneshvishnole <purneshvishnole@gmail.com>
2023-12-30 14:39:40 +0530

    till 3 star rating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Accordian from "./components/Accordian/index.jsx";
 import RandomColor from "./components/random-color/index.jsx";
+import StarRating from "./components/star-rating/index.jsx";
 
 const App = () => {
   return (
     <>
       <Accordian />
       <RandomColor />
+      <StarRating noOfStars={10} />
     </>
   );
 };
